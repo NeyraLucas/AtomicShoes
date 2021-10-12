9d98511db776b1e42dd0fd538970a07575b4b13e
NeyraLucas <luiscrazypeople@gmail.com>
2021-10-12 14:37:42 -0500

    add carrusel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+
     <div class="container py-8">
         <section>
             <h1 class="text-lg uppercase font-bold text-gray-700">
@@ -9,29 +10,45 @@
         </section>
     </div>
 
-{{-- carousel --}}
+    <div class="container py-8">
+        @livewire('carousel-banners')
+    </div>
+
+    {{-- carousel --}}
+
+    
 
     @push('script')
+        <script>
+            Livewire.on('glider', function() {
+
+                new Glider(document.querySelector('.glider'), {
+                    slidesToShow: 5.5,
+                    slidesToScroll: 5,
+                    draggable: true,
+                    dots: '.dots',
+                    arrows: {
+                        prev: '.glider-prev',
+                        next: '.glider-next'
+                    }
+                });
+
+            });
+        </script>
+    @endpush
+
     <script>
-
-        Livewire.on('glider', function(){
-
-            new Glider(document.querySelector('.glider'), {
-            slidesToShow: 5.5,
-            slidesToScroll: 5,
+        new Glider(document.querySelector('.banner'), {
+            slidesToShow: 1,
+            dots: '#dots',
             draggable: true,
-            dots: '.dots',
             arrows: {
                 prev: '.glider-prev',
                 next: '.glider-next'
             }
         });
-
-        })
-
-        
     </script>
-    @endpush
 
-    
+
+
 </x-app-layout>
